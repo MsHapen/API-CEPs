9d1691f1418e3170b6bbd690b5157e593687b9ca
MsHapen <99226288+MsHapen@users.noreply.github.com>
2022-09-21 09:17:17 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+export default {
+data() {
+  return {
+    cep: "",
+    endereço: {},
+  };
+},
+methods: {
+  async buscar() {
+    const url = `https://www.viacep.com.br/ws/$(this.cep)/json/`;
+    const res = await axios.get(url);
+    this.endereço = res.data;
+  }
+},
+};
+</script>
+
+<template>
+  <main>
+    <h1>Busca CEP</h1>
+    <form @submit.prevent="buscar"></form>
+  </main>
+</template>
